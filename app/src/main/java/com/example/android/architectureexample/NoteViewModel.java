04fbc7f18149ded6e541f35b138cc483496c8636
Tarek <tarek.buhdeima@gmail.com>
2018-09-20 22:34:00 +0200

    finished classes Note, NoteDao, NoteDatabase, NoteRepository, NoteViewModel

---
+++ app/src/main/java/com/example/android/architectureexample/NoteViewModel.java
@@ -0,0 +1,42 @@
+package com.example.android.architectureexample;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+/**
+ * Created by Tarek on 20-Sep-18.
+ */
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository repository;
+
+    private LiveData<List<Note>> allNotes;
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    public void insert(Note note) {
+        repository.insert(note);
+    }
+
+    public void update(Note note) {
+        repository.update(note);
+    }
+
+    public void delete(Note note) {
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+}
