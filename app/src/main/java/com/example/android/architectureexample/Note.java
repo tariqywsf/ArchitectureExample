e06d623b0ebaf9b92004abc9c42cf5a7a55b01fa
Tarek <tarek.buhdeima@gmail.com>
2018-10-06 05:43:56 +0200

    migrated to androidx

--- app/src/main/java/com/example/android/architectureexample/Note.java
+++ app/src/main/java/com/example/android/architectureexample/Note.java
@@ -1,7 +1,7 @@
 package com.example.android.architectureexample;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 /**
  * Created by Tarek on 20-Sep-18.
